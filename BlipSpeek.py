13a4f1b0a7c67f9ad0995e0b7599716469ad24be
NathanThai2201 <thainhatanh2201@gmail.com>
2024-09-04 04:21:24 -0600

    update

--- BlipSpeek.py
+++ BlipSpeek.py
@@ -1,13 +1,35 @@
 import simpleaudio as sa
 import os
+import threading
+import time
 
-global_ringtone = "ring1.wav"
+# GLOBAL VARIABLES, ONLY CHANGE THESE
+global_tone = "1.wav"
+global_speed = 0.05
 
-def main():
-    sound_path = os.path.join(os.path.dirname(__file__), 'sounds', global_ringtone)
+def playsound(end = 0):
+    sound_path = os.path.join(os.path.dirname(__file__), 'sounds', global_tone)
     wave_obj = sa.WaveObject.from_wave_file(sound_path)
     play_obj = wave_obj.play()
-    play_obj.wait_done()  # Wait until sound has finished playing
+    if end == 1:
+        play_obj.wait_done()
+
+def main():
+    os.system('cls' if os.name=='nt' else 'clear')
+    print("Enter a sentence:")
+    print("")
+    text_input = input('Input:').lower()
+    os.system('cls' if os.name=='nt' else 'clear')
+
+    words = text_input.split(' ')
+    for word in words:
+        print(word)
+        for i in range(len(word)):
+            if i == len(word)-1:
+                playsound(1)
+            else:
+                playsound(0)
+            time.sleep(global_speed)
 
 if __name__ == "__main__":
     main()
